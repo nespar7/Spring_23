2d18c04acc8d804191a276904fc62ac06cd213e4
nespar7 <nsuryaprakashreddy1234@gmail.com>
2023-01-10 21:44:17 +0530

    prettify time_clien.c

--- Networks_Lab/Assignment_1/Question_1/time_client.c
+++ Networks_Lab/Assignment_1/Question_1/time_client.c
@@ -3,13 +3,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define SERVER_PORT 20000
 
-int main(){
+int main()
+{
     int sockfd;
     int len;
     char buff[100];
@@ -19,7 +20,8 @@ int main(){
 
     // Open a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
+    if (sockfd < 0)
+    {
         perror("Cannot create socket!");
         exit(0);
     }
@@ -31,9 +33,10 @@ int main(){
     addr.sin_addr.s_addr = INADDR_ANY;
 
     // Connecting to server
-    response = connect(sockfd, (struct sockaddr*) &addr, sizeof(addr));
+    response = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
 
-    if(response < 0){
+    if (response < 0)
+    {
         perror("Connection to server failed");
         close(sockfd);
         exit(0);
@@ -41,8 +44,9 @@ int main(){
     printf("Connected Successfully!\n");
 
     // Receiving the time data as a string from the server
-    response = recv(sockfd, buff, sizeof(buff)+1, 0);
-    if(response < 0){
+    response = recv(sockfd, buff, sizeof(buff) + 1, 0);
+    if (response < 0)
+    {
         perror("Receive failed");
         close(sockfd);
         exit(0);

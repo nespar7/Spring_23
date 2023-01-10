38b8bcc843f2d993d84f45bac71d57082cc5043d
nespar7 <nsuryaprakashreddy1234@gmail.com>
2023-01-10 21:42:58 +0530

    prettify time_server.c

--- Networks_Lab/Assignment_1/Question_1/time_server.c
+++ Networks_Lab/Assignment_1/Question_1/time_server.c
@@ -1,4 +1,4 @@
-/**********************************************************************************************/ 
+/**********************************************************************************************/
 /* References:                                                                                */
 /*  https://www.ibm.com/docs/en/i/7.1?topic=designs-example-writing-iterative-server-program  */
 /*                                                                                            */
@@ -11,15 +11,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <time.h>
 
 #define SERVER_PORT 20000
 
-int main(){
-    int sockfd, newsockfd; 
+int main()
+{
+    int sockfd, newsockfd;
     int clilen;
     int response;
     struct sockaddr_in serv_addr, cli_addr;
@@ -29,7 +30,8 @@ int main(){
 
     // opening a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
+    if (sockfd < 0)
+    {
         perror("Cannot create socket");
         exit(0);
     }
@@ -41,8 +43,9 @@ int main(){
     serv_addr.sin_addr.s_addr = INADDR_ANY;
 
     // Binding the server address to the socket
-    response = bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr));
-    if(response < 0){
+    response = bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (response < 0)
+    {
         perror("Unable to bind to localhost");
         close(sockfd);
         exit(0);
@@ -51,7 +54,8 @@ int main(){
 
     // We listen to upto 5 servers at a time in the queue
     response = listen(sockfd, 5);
-    if(response < 0){
+    if (response < 0)
+    {
         perror("Failed to listen");
         close(sockfd);
         exit(0);
@@ -59,13 +63,15 @@ int main(){
     printf("Listening...\n");
 
     // iterating
-    while(1){
+    while (1)
+    {
         clilen = sizeof(cli_addr);
 
         // Accepting a client connection
-        newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
 
-        if(newsockfd < 0){
+        if (newsockfd < 0)
+        {
             perror("Accept failed");
             exit(0);
         }
@@ -78,9 +84,10 @@ int main(){
         // No receive call because the client does not send any data
 
         // Sending the server time message
-        response = send(newsockfd, buff, strlen(buff)+1, 0);
+        response = send(newsockfd, buff, strlen(buff) + 1, 0);
 
-        if(response < 0){
+        if (response < 0)
+        {
             perror("Failed to send the message");
             close(newsockfd);
             exit(0);

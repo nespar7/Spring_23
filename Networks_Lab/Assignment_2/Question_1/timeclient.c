97fd71aa4a0337abdaff91eb70a4e9b4fe90b6e3
nespar7 <nsuryaprakashreddy1234@gmail.com>
2023-01-15 10:55:16 +0530

    Question 1

---
+++ Networks_Lab/Assignment_2/Question_1/timeclient.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+
+#define PORT 20000
+#define BUFFSIZE 100
+
+int main()
+{
+    int sockfd;
+    char buffer[BUFFSIZE];
+    char *message = "Requesting server time";
+    struct sockaddr_in servaddr;
+    int response;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Cannot create socket");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+
+    int len;
+    len = sizeof(servaddr);
+
+    sendto(sockfd, (const char *)message, strlen(message), 0, (struct sockaddr *)&servaddr, len);
+    printf("Message sent: %s\n", message);
+
+    for (int i = 0; i < 5; i++)
+    {
+        struct pollfd setfd;
+
+        setfd.fd = sockfd;
+        setfd.events = POLLIN;
+
+        response = poll(&setfd, 1, 3000);
+
+        if (response < 0)
+        {
+            perror("Poll failed");
+            exit(EXIT_FAILURE);
+        }
+        else if (response == 0)
+        {
+            continue;
+        }
+        else
+        {
+            response = recvfrom(sockfd, (char *)buffer, BUFFSIZE, 0, (struct sockaddr *)&servaddr, &len);
+            printf("Server time: %s", buffer);
+            break;
+        }
+    }
+
+    if(response == 0){
+        printf("Timeout exceeded\n");
+    }
+
+    close(sockfd);
+
+    return 0;
+}
--- Networks_Lab/Assignment_2/Question_1/timeserver.c
+++ Networks_Lab/Assignment_2/Question_1/timeserver.c
@@ -1,64 +1,73 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <time.h>
 
-int main(){
+#define PORT 20000
+#define BUFFSIZE 100
+
+int main()
+{
     int sockfd;
+    char buff[BUFFSIZE];
+    char *message;
     struct sockaddr_in servaddr, cliaddr;
     int response;
-    char buff[50];
 
+    // Creating a UDP socket
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sockfd < 0){
-        perror("Cannot create socket");
-        exit(0);
+    if (sockfd < 0)
+    {
+        perror("Could not create socket");
+        exit(EXIT_FAILURE);
     }
-    printf("Socket created\n");
+    printf("Created socket!\n");
 
     memset(&servaddr, 0, sizeof(servaddr));
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(20000);
+    servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
 
-    response = bind(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
-    if(response < 0){
-        perror("Unable to bind");
-        close(sockfd);
-        exit(0);
+    // Binding the socket to local address, port 20000
+    response = bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (response < 0)
+    {
+        perror("Could not bind");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Server running...\n");
+    while (1)
+    {
+        socklen_t len;
+        len = sizeof(cliaddr);
 
-    while(1){
-        socklen_t clilen = sizeof(cliaddr);
-        response = recvfrom(sockfd, (char *)buff, 50, 0, (struct sockaddr*)&cliaddr, &clilen);
-        if(response < 0){
-            perror("Could not receive datagram");
-            exit(0);
+        // Receiving a datagram from the client
+        response = recvfrom(sockfd, (char *)buff, BUFFSIZE, 0, (struct sockaddr *)&cliaddr, &len);
+        if (response < 0)
+        {
+            perror("Could not receive");
+            exit(EXIT_FAILURE);
         }
-        printf("Client sent: %s\n", buff);
+        buff[response] = '\0';
+        printf("Received from client: %s\n", buff);
 
-        // Storing server time in buff
+        // Storing the server time in message
         time_t t;
         t = time(NULL);
-        snprintf(buff, sizeof(buff), "%s", ctime(&t));
+        message = ctime(&t);
 
-        response = sendto(sockfd, (const char *)buff, strlen(buff), 0, (struct sockaddr*)&cliaddr, clilen);
-        if(response < 0){
-            perror("Could not send data");
-            exit(0);
-        }
+        // Sending the time to client
+        response = sendto(sockfd, message, strlen(message), 0, (const struct sockaddr *)&cliaddr, len);
     }
-
+    
     close(sockfd);
+
     return 0;
 }
